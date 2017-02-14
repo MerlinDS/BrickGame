a65350cb0c26bdf85a41e66e15de956ac6c09d93
merlinds <merlinkolv@gmail.com>
2017-02-14 18:41:32 +0200

    Added fps manager

---
+++ Assets/BrickGame/Scripts/UI/FPSIndicator.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="FPSIndicator.cs" company="Near Fancy">
+// Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author>Andrew Salomatin</author>
+// <date>02/14/2017 18:31</date>
+// ReSharper disable InconsistentNaming
+
+using BrickGame.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BrickGame.Scripts.UI
+{
+    /// <summary>
+    /// FPSIndicator
+    /// </summary>
+    [RequireComponent(typeof(Text))]
+    public class FPSIndicator : GameBehaviour
+    {
+        private const string Display = "{0} FPS";
+        //================================       Public Setup       =================================
+
+        //================================    Systems properties    =================================
+        private int _pastFrame;
+        private Text _testField;
+        private IFPSCounter _fpsCounter;
+        //================================      Public methods      =================================
+
+        //================================ Private|Protected methods ================================
+        private void Start()
+        {
+            _testField = GetComponent<Text>();
+            _fpsCounter = Context.GetActor<FPSManager>();
+        }
+
+        private void LateUpdate()
+        {
+            if (_pastFrame == _fpsCounter.FPS) return;
+            _testField.text = string.Format(Display, _fpsCounter.FPS);
+            _pastFrame = _fpsCounter.FPS;
+        }
+    }
+}
---
+++ Assets/BrickGame/Scripts/Utils/FPSManager.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="FPSManager.cs" company="Near Fancy">
+// Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author>Andrew Salomatin</author>
+// <date>02/14/2017 18:22</date>
+
+using MiniMoca;
+using UnityEngine;
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+namespace BrickGame.Scripts.Utils
+{
+    public interface IFPSCounter : IMocaActor
+    {
+        /// <summary>
+        /// Get current FPS
+        /// </summary>
+        int FPS { get; }
+    }
+    /// <summary>
+    /// FPSManager - Simple fps counter
+    /// </summary>
+    public class FPSManager : GameManager, IFPSCounter
+    {
+        //================================       Public Setup       =================================
+        /// <inheritdoc />
+        public int FPS
+        {
+            get { return _currentFps; }
+        }
+
+        //================================    Systems properties    =================================
+        const float FPSMeasurePeriod = 0.5f;
+        private int _fpsAccumulator;
+        private float _fpsNextPeriod;
+        private int _currentFps;
+        //================================      Public methods      =================================
+
+        //================================ Private|Protected methods ================================
+        private void Start()
+        {
+            _fpsNextPeriod = Time.realtimeSinceStartup + FPSMeasurePeriod;
+        }
+
+        private void Update()
+        {
+            // measure average frames per second
+            _fpsAccumulator++;
+            if (Time.realtimeSinceStartup > _fpsNextPeriod)
+            {
+                _currentFps = (int) (_fpsAccumulator/FPSMeasurePeriod);
+                _fpsAccumulator = 0;
+                _fpsNextPeriod += FPSMeasurePeriod;
+            }
+}
+    }
+}
