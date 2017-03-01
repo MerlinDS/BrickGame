2b3e62af2c395a528b270a3dea09044856d9d20c
merlinds <merlinkolv@gmail.com>
2017-03-01 12:18:48 +0200

    Added music, changed buttons images

--- Assets/BrickGame/Scripts/UI/SoundButton.cs
+++ Assets/BrickGame/Scripts/UI/SoundButton.cs
@@ -4,29 +4,41 @@
 // <author>Andrew Salomatin</author>
 // <date>02/14/2017 18:17</date>
 
+using BrickGame.Scripts.Controllers;
 using BrickGame.Scripts.UI.Components;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BrickGame.Scripts.UI
 {
     /// <summary>
     /// SoundButton
     /// </summary>
+    [RequireComponent(typeof(Image))]
     public class SoundButton : GameControlsButton
     {
         //================================       Public Setup       =================================
-
+        public Sprite On;
+        public Sprite Off;
         //================================    Systems properties    =================================
-
+        private Image _image;
         //================================      Public methods      =================================
 
         //================================ Private|Protected methods ================================
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+            if (Context.GetActor<AudioController>().Muted)
+                _image.sprite = Off;
+        }
+
         /// <inheritdoc />
         protected override void OnClickHandler()
         {
             Debug.Log("Change sound mode");
             BroadcastNofitication(GameNotification.MuteSound);
-            //TODO: Change icon
+            _image.sprite = _image.sprite == On ? Off : On;
+
         }
     }
 }
